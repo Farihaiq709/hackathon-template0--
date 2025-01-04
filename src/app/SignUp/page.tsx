6488947dcb6daeb65ecf61f910807856fb44a04d
Farihaiq709 <160820315+Farihaiq709@users.noreply.github.com>
2025-01-04 13:55:45 +0500

    Add files via upload

---
+++ src/app/SignUp/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image"
+export default function SignUp() {
+    return(
+        <div>
+            <div className="w-[1305px] h-[781px] gap-[129px] flex  ">
+                <div className="w-[805px]">
+                    <div className="w-[919px] h-[706px] relative left-[-8px] ">
+                        <Image className="" src={'/newimage.png'} alt=""width={919} height={706} />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
